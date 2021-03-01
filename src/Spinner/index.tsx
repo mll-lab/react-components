a75f708f5ac368e33c1a7a8434b596837144c6a5
Benedikt Franke <benedikt.franke@mll.com>
2021-03-01 14:00:15 +0100

    feat: Add Spinner component

---
+++ src/Spinner/index.tsx
@@ -0,0 +1,31 @@
+import { Spin } from 'antd';
+import { SpinProps } from 'antd/es/spin';
+import React, { ReactNode } from 'react';
+
+import { MllSpinnerIcon } from './MllSpinnerIcon';
+
+export * from './MllSpinnerIcon';
+
+export type SpinnerProps = Omit<SpinProps, 'size' | 'tip'> & {
+  size?: string;
+  message?: string;
+  children?: ReactNode;
+};
+
+export function Spinner({ message, size = '2em', ...spinProps }: SpinnerProps) {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      {message && <h2>{message}</h2>}
+      <Spin
+        indicator={
+          <MllSpinnerIcon
+            style={{
+              width: size,
+            }}
+          />
+        }
+        {...spinProps}
+      />
+    </div>
+  );
+}
