7664a235f8ff87af9d7e2e5940b7605b4987dfed
Benedikt Franke <benedikt.franke@mll.com>
2021-03-01 14:00:15 +0100

    feat: Add Spinner component

---
+++ src/Spinner/index.stories.tsx
@@ -0,0 +1,12 @@
+import { Story } from '@storybook/react';
+import React from 'react';
+
+import { Spinner, SpinnerProps } from './index';
+
+export default {
+  title: 'Spinner',
+  component: Spinner,
+};
+
+export const Default: Story<SpinnerProps> = (args
+) => <Spinner {...args} />;
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import './antd.less';
 export * from './theme';
 export * from './Button';
 export * from './Provider';
+export * from './Spinner';
