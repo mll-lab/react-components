405d08299eb48ba49527ce5cc8843916b72eea0e
Benedikt Franke <benedikt.franke@mll.com>
2021-03-04 14:35:16 +0100

    fix: Prevent antd from polluting global styles

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -6,17 +6,23 @@ import { MLL_THEME, MllTheme } from './theme';
 
 type ProviderProps = { theme?: MllTheme };
 
+const PREFIX_CLS = 'mll-ant';
+
 export function Provider({
   children,
   theme,
 }: PropsWithChildren<ProviderProps>) {
   AntdConfigProvider.config({
-    prefixCls: 'mll-ant',
+    prefixCls: PREFIX_CLS,
   });
 
   return (
     <ThemeProvider theme={theme ?? MLL_THEME}>
-      <AntdConfigProvider prefixCls="mll-ant">{children}</AntdConfigProvider>
+      <AntdConfigProvider prefixCls={PREFIX_CLS}>
+        <div id={PREFIX_CLS}>
+          {children}
+        </div>
+      </AntdConfigProvider>
     </ThemeProvider>
   );
 }
