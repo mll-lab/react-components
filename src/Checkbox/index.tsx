93cf6650d91bc12ade4d88e96a1ed3fba760c725
Benedikt Franke <benedikt.franke@mll.com>
2021-03-04 14:51:49 +0100

    fix: Add explicit types to reexported types

--- src/Checkbox/index.tsx
+++ src/Checkbox/index.tsx
@@ -1,14 +1,3 @@
 import { Checkbox as AntdCheckbox } from 'antd';
-import { CheckboxProps } from 'antd/es/checkbox/Checkbox';
-import { CheckboxGroupProps } from 'antd/es/checkbox/Group';
-import * as React from 'react';
 
-interface CheckboxComponentType
-  extends React.ForwardRefExoticComponent<
-    CheckboxProps & React.RefAttributes<HTMLInputElement>
-  > {
-  Group: React.NamedExoticComponent<CheckboxGroupProps>;
-  __ANT_CHECKBOX: boolean;
-}
-
-export const Checkbox: CheckboxComponentType = AntdCheckbox;
+export const Checkbox: typeof AntdCheckbox = AntdCheckbox;
