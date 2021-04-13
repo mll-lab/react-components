42f5c9d1051da530f8e2a8b8707b19e5349db83a
Benedikt Franke <benedikt.franke@mll.com>
2021-04-13 15:51:01 +0200

    refactor: Organize exports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 // eslint-disable-next-line @mll-lab/no-global-styles
 import './antd.less';
 
+// Global configuration
 export * from './theme';
+export * from './Provider';
+
+// Individual components
 export * from './Button';
 export * from './Checkbox';
 export * from './Empty';
 export * from './Input';
 export * from './List';
 export * from './Radio';
-export * from './Tabs';
-export * from './Provider';
 export * from './Spinner';
-export * from './Tooltip';
 export * from './Table';
+export * from './Tabs';
+export * from './Tooltip';
