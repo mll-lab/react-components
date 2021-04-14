c22b5512a17384cb1a5c9f6d901ec618059aea2a
SimBig <SimBig@users.noreply.github.com>
2021-04-14 15:40:05 +0000

    refactor: Apply eslint fix

--- src/Skeleton/index.tsx
+++ src/Skeleton/index.tsx
@@ -1,4 +1,7 @@
-import { Skeleton as AntdSkeleton, SkeletonProps as AntdSkeletonProps } from 'antd';
+import {
+  Skeleton as AntdSkeleton,
+  SkeletonProps as AntdSkeletonProps,
+} from 'antd';
 
 export const Skeleton: typeof AntdSkeleton = AntdSkeleton;
 export type SkeletonProps = AntdSkeletonProps;
