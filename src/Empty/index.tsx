00075d8cb1f75e7a35ab8c40483de1aa82ab249a
Benedikt Franke <benedikt.franke@mll.com>
2021-04-14 10:55:52 +0200

    fix: Reexport EmptyType

--- src/Empty/index.tsx
+++ src/Empty/index.tsx
@@ -1,3 +1,12 @@
 import { Empty as AntdEmpty } from 'antd';
+import { EmptyProps } from 'antd/lib/empty';
+import { FC, ReactNode } from 'react';
 
-export const Empty: typeof AntdEmpty = AntdEmpty;
+// We have to redefine this in order to avoid the TypeScript error:
+// TS4082: Default export of the module has or is using private name 'EmptyType'.
+export type EmptyType = {
+  PRESENTED_IMAGE_DEFAULT: ReactNode;
+  PRESENTED_IMAGE_SIMPLE: ReactNode;
+} & FC<EmptyProps>;
+
+export const Empty: EmptyType = AntdEmpty;
