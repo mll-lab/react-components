5d3d761a60c4dfc7be589108ae8502bc97ad94e1
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-04-19 18:01:26 +0200

    feat: Add Alert component

---
+++ src/Alert/index.stories.tsx
@@ -0,0 +1,34 @@
+import { Story } from '@storybook/react';
+import React from 'react';
+
+import { Alert, AlertProps } from './index';
+
+export default {
+  title: 'Alert',
+  argTypes: {
+    onClose: { action: 'clicked' },
+    closable: {
+      control: {
+        type: 'boolean',
+        checked: false,
+      },
+    },
+    showIcon: {
+      control: {
+        type: 'boolean',
+        checked: false,
+      },
+    },
+    type: {
+      control: {
+        type: 'inline-radio',
+        options: ['success', 'info', 'warning', 'error'],
+        default: 'success',
+      },
+    },
+  },
+};
+
+export const Default: Story<AlertProps> = (args) => (
+  <Alert message="Warning Text" type="warning" closable showIcon {...args} />
+);
---
+++ src/Alert/index.tsx
@@ -0,0 +1,4 @@
+import { Alert as AntdAlert, AlertProps as AntdAlertProps } from 'antd';
+
+export const Alert: typeof AntdAlert = AntdAlert;
+export type AlertProps = AntdAlertProps;
