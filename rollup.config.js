8b3d2c2600e24b30df3b5aada8f121ba8770784a
Benedikt Franke <benedikt.franke@mll.com>
2021-04-19 11:43:55 +0200

    build: Update dependencies and resolve this to undefined warning

--- rollup.config.js
+++ rollup.config.js
@@ -14,6 +14,16 @@ export default {
       sourcemap: true,
     },
   ],
+
+  // this resolves to window in the browser, thus enabling caching in the antd code below
+  // (!) `this` has been rewritten to `undefined`
+  // https://rollupjs.org/guide/en/#error-this-is-undefined
+  //     node_modules/antd/es/affix/index.js
+  // 7: import _typeof from "@babel/runtime/helpers/esm/typeof";
+  // 8:
+  // 9: var __decorate = this && this.__decorate || function (decorators, target, key, desc) {
+  context: 'this',
+
   plugins: [
     peerDepsExternal(),
     resolve(),
