93b77b0f1293af8a4cf8f8d41ce971211e17d931
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-04-23 13:35:00 +0200

    feat: Add Steps component

---
+++ src/Steps/index.stories.tsx
@@ -0,0 +1,21 @@
+import { Story } from '@storybook/react';
+import React from 'react';
+const { Step } = Steps;
+
+import { Steps, StepsProps } from './index';
+
+export default {
+  title: 'Steps',
+};
+
+export const Simple: Story<StepsProps> = (args) => (
+    <Steps current={1} {...args}>
+        <Step title="Finished" description="This is a description." />
+        <Step title="In Progress" subTitle="Left 00:00:08" description="This is a description." />
+        <Step title="Waiting" description="This is a description." />
+    </Steps>
+);
+Simple.argTypes = {
+    current: { control: { type: 'text' } },
+};
+
---
+++ src/Steps/index.tsx
@@ -0,0 +1,4 @@
+import { Steps as AntdSteps, StepsProps as AntdStepsProps } from 'antd';
+
+export const Steps: typeof AntdSteps = AntdSteps;
+export type StepsProps = AntdStepsProps;
