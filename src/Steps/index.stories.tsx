a9a9e456d71c1f535d7cdf63c9fd75f60332f2ea
SimBig <SimBig@users.noreply.github.com>
2021-04-23 12:31:46 +0000

    refactor: Apply eslint fix

--- src/Steps/index.stories.tsx
+++ src/Steps/index.stories.tsx
@@ -1,21 +1,25 @@
 import { Story } from '@storybook/react';
 import React from 'react';
-const { Step } = Steps;
 
 import { Steps, StepsProps } from './index';
+
+const { Step } = Steps;
 
 export default {
   title: 'Steps',
 };
 
 export const Simple: Story<StepsProps> = (args) => (
-    <Steps current={1} {...args}>
-        <Step title="Finished" description="This is a description." />
-        <Step title="In Progress" subTitle="Left 00:00:08" description="This is a description." />
-        <Step title="Waiting" description="This is a description." />
-    </Steps>
+  <Steps current={1} {...args}>
+    <Step title="Finished" description="This is a description." />
+    <Step
+      title="In Progress"
+      subTitle="Left 00:00:08"
+      description="This is a description."
+    />
+    <Step title="Waiting" description="This is a description." />
+  </Steps>
 );
 Simple.argTypes = {
-    current: { control: { type: 'text' } },
+  current: { control: { type: 'text' } },
 };
-
