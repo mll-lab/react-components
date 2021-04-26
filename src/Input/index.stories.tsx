27f3522f30dfc4cc5ad304bf120bf81919da7f57
SimBig <SimBig@users.noreply.github.com>
2021-04-26 06:48:42 +0000

    refactor: Apply eslint fix

--- src/Input/index.stories.tsx
+++ src/Input/index.stories.tsx
@@ -13,5 +13,5 @@ export default {
 export const String: Story<InputProps> = (args) => <Input {...args} />;
 
 export const Number: Story<InputNumberProps> = (args) => (
-  <InputNumber min={1} max={10} defaultValue={3} {...args}/>
+  <InputNumber min={1} max={10} defaultValue={3} {...args} />
 );
