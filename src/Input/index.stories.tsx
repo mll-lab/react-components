11e81d937dfd9e3ea324728989e1de93f69e477c
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-04-26 08:47:00 +0200

    feat: Add InputNumber component

---
+++ src/Input/index.stories.tsx
@@ -0,0 +1,17 @@
+import { Story } from '@storybook/react';
+import { InputNumber } from 'antd';
+import React from 'react';
+
+import { Input, InputNumberProps, InputProps } from './index';
+
+export default {
+  title: 'Input',
+  component: Input,
+  onChange: { action: 'clicked' },
+};
+
+export const String: Story<InputProps> = (args) => <Input {...args} />;
+
+export const Number: Story<InputNumberProps> = (args) => (
+  <InputNumber min={1} max={10} defaultValue={3} {...args}/>
+);
--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -1,3 +1,8 @@
-import { Input as AntdInput } from 'antd';
+import { Input as AntdInput, InputNumber as AntdInputNumber } from 'antd';
+import { InputProps as AntdInputProps } from 'antd/lib/input';
+import { InputNumberProps as AntdInputNumberProps } from 'antd/lib/input-number';
 
 export const Input: typeof AntdInput = AntdInput;
+export const InputNumber: typeof AntdInputNumber = AntdInputNumber;
+export type InputProps = AntdInputProps;
+export type InputNumberProps = AntdInputNumberProps;
