6d39ac650da3d67b23342e35b88f2e3c248cb597
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-04-30 10:27:22 +0200

    feat: Add popconfirm component

---
+++ src/Popconfirm/index.tsx
@@ -0,0 +1,7 @@
+import {
+  Popconfirm as AntdPopconfirm,
+  PopconfirmProps as AntdPopconfirmProps,
+} from 'antd';
+
+export const Popconfirm: typeof AntdPopconfirm = AntdPopconfirm;
+export type PopconfirmProps = AntdPopconfirmProps;
