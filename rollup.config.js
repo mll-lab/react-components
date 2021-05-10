8320a05ed9b61f9fa0ba2f46fe13ea3ce69e9389
Benedikt Franke <benedikt.franke@mll.com>
2021-05-10 14:27:12 +0200

    feat: Publish commonjs build

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,19 @@ import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import styles from 'rollup-plugin-styles';
 
+// eslint-disable-next-line import/extensions
+import pkg from './package.json';
+
 export default {
   input: 'src/index.ts',
   output: [
     {
-      dir: 'dist',
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
       format: 'esm',
       sourcemap: true,
     },
