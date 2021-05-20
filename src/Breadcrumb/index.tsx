9fc6cd6afb30527a5ecd17b1c7792c78a277fdc6
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-05-20 17:17:19 +0200

    feat: Add Breadcrumb component

---
+++ src/Breadcrumb/index.tsx
@@ -0,0 +1,7 @@
+import {
+  Breadcrumb as AntdBreadcrumb,
+  BreadcrumbProps as AntdBreadcrumbProps,
+} from 'antd';
+
+export const Breadcrumb: typeof AntdBreadcrumb = AntdBreadcrumb;
+export type BreadcrumbProps = AntdBreadcrumbProps;
