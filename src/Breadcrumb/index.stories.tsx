982a50140f52dd3ce35e0979758bd69fbcca4dda
SimBig <SimBig@users.noreply.github.com>
2021-05-21 06:51:26 +0000

    refactor: Apply eslint fix

--- src/Breadcrumb/index.stories.tsx
+++ src/Breadcrumb/index.stories.tsx
@@ -1,8 +1,8 @@
+import { HomeOutlined } from '@ant-design/icons';
 import { Story } from '@storybook/react';
 import React from 'react';
 
 import { Breadcrumb, BreadcrumbProps } from './index';
-import {HomeOutlined} from "@ant-design/icons";
 
 export default {
   title: 'Breadcrumb',
@@ -10,7 +10,9 @@ export default {
 
 export const Default: Story<BreadcrumbProps> = (args) => (
   <Breadcrumb {...args}>
-    <Breadcrumb.Item><HomeOutlined /></Breadcrumb.Item>
+    <Breadcrumb.Item>
+      <HomeOutlined />
+    </Breadcrumb.Item>
     <Breadcrumb.Item>
       <a href="">Application Center</a>
     </Breadcrumb.Item>
