36e98a7f41084be2f054f2ce227f81ba0cc9175d
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-05-28 11:18:20 +0200

    feat: Add DatePicker component

---
+++ src/DatePicker/index.tsx
@@ -0,0 +1,70 @@
+import { addYears } from 'date-fns';
+import localeDe from 'date-fns/locale/de';
+import React from 'react';
+import {
+  ReactDatePickerProps,
+  registerLocale,
+  setDefaultLocale,
+} from 'react-datepicker';
+
+import {
+  GERMAN_DATE_FORMAT,
+  GERMAN_DATE_TIME_FORMAT,
+  parseGermanDateFlexible,
+  MINIMAL_VALID_DATE,
+} from '../utils/date';
+
+import { StyledDatePicker } from './StyledDatePicker';
+
+registerLocale('de', localeDe);
+setDefaultLocale('de');
+
+export type DatePickerProps = Omit<ReactDatePickerProps, 'onChangeRaw'>;
+
+export default function DatePicker(props: DatePickerProps) {
+  return (
+    <StyledDatePicker
+      {...props}
+      showYearDropdown
+      showMonthDropdown
+      scrollableYearDropdown
+      minDate={MINIMAL_VALID_DATE}
+      maxDate={addYears(new Date(), 1)}
+      yearDropdownItemNumber={50}
+      timeCaption="Zeit"
+      dateFormat={
+        props.showTimeSelect ? GERMAN_DATE_TIME_FORMAT : GERMAN_DATE_FORMAT
+      }
+      // We can only control how the DatePicker formats dates, but not how it parses
+      // keyboard input. We can listen to the raw change events and attempt to parse
+      // a date out of the input ourselves
+      onChangeRaw={(event) => {
+        const rawInput = event.target.value;
+
+        // Allow clearing the date
+        if (rawInput === '') {
+          props.onChange(null, event);
+
+          return;
+        }
+
+        const inputDate = parseGermanDateFlexible(rawInput);
+        // Only propagate the change to redux if the user actually entered a valid date
+        if (inputDate) {
+          props.onChange(inputDate, event);
+        }
+      }}
+      onChange={(date, event) => {
+        // The user actually used the date picker to select a date.
+        if (event?.type === 'click') {
+          // In this case, we know the input is a valid, unambiguous date instance,
+          // so we just pass it as is.
+          props.onChange(date, event);
+        } else {
+          // Do nothing, this case is handled by onChangeRaw
+        }
+      }}
+      customInput={<input data-testid="date-picker" type="text" />}
+    />
+  );
+}
