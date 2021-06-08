36b954b9a7be5c55bb26a94fc7cacc4f8bacb9e4
SimBig <SimBig@users.noreply.github.com>
2021-06-08 09:35:57 +0000

    refactor: Apply eslint fix

--- src/Upload/index.stories.tsx
+++ src/Upload/index.stories.tsx
@@ -1,16 +1,17 @@
+import { UploadOutlined } from '@ant-design/icons';
 import { Story } from '@storybook/react';
 import React from 'react';
 
+import { Button } from '../Button';
+
 import { Upload, UploadProps } from './index';
-import {Button} from "../Button";
-import { UploadOutlined } from '@ant-design/icons';
 
 export default {
   title: 'Upload',
 };
 
 export const Default: Story<UploadProps> = (args) => (
-    <Upload {...args}>
-        <Button icon={<UploadOutlined />}>Click to Upload</Button>
-    </Upload>
+  <Upload {...args}>
+    <Button icon={<UploadOutlined />}>Click to Upload</Button>
+  </Upload>
 );
