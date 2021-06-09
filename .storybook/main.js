158c5845ddc721a1d603f34788c4ec561aec2e38
Benedikt Franke <benedikt.franke@mll.com>
2021-06-09 12:20:22 +0200

    ci: Add css loader to fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -37,6 +37,13 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
     ];
 
     return config;
