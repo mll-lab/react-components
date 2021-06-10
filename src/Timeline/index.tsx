41cd9d1937e193735f319cffe385e4d9177f2953
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-06-10 08:40:54 +0200

    feat: Add Timeline component

---
+++ src/Timeline/index.tsx
@@ -0,0 +1,4 @@
+import { Timeline as AntdTimeline, TimelineProps as AntdTimelineProps } from 'antd';
+
+export const Timeline: typeof AntdTimeline = AntdTimeline;
+export type TimelineProps = AntdTimelineProps;
