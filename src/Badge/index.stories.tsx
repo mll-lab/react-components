7f43943bbdf76f354022911159e1c98253c75ad0
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-07-08 12:02:10 +0200

    feat: Add Badge component

---
+++ src/Badge/index.stories.tsx
@@ -0,0 +1,17 @@
+import { BellOutlined } from '@ant-design/icons';
+import { Story } from '@storybook/react';
+import React from 'react';
+
+import { Badge, BadgeProps } from './index';
+
+export default {
+  title: 'Badge',
+};
+
+export const Default: Story<BadgeProps> = (args) => (
+  <div>
+    <Badge size={'small'} count={5} offset={[5, 0]} {...args}>
+      <BellOutlined style={{ fontSize: '130%' }} />
+    </Badge>
+  </div>
+);
---
+++ src/Badge/index.tsx
@@ -0,0 +1,4 @@
+import { Badge as AntdBadge, BadgeProps as AntdBadgeProps } from 'antd';
+
+export const Badge: typeof AntdBadge = AntdBadge;
+export type BadgeProps = AntdBadgeProps;
