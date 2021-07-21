25ab1419de943517edce7912a47d96f603e4d792
Benedikt Franke <benedikt.franke@mll.com>
2021-07-21 17:42:47 +0200

    With prefix

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,3 +1,4 @@
+import { ConfigProvider as AntdConfigProvider } from 'antd';
 import React, { PropsWithChildren } from 'react';
 import { ThemeProvider } from 'styled-components';
 
@@ -5,11 +6,23 @@ import { MLL_THEME, MllTheme } from './theme';
 
 type ProviderProps = { theme?: Partial<MllTheme> };
 
+export const PREFIX_CLS = 'mll-ant';
+
 export function Provider({
   children,
   theme,
 }: PropsWithChildren<ProviderProps>) {
+  AntdConfigProvider.config({
+    prefixCls: PREFIX_CLS,
+  });
+
   return (
-    <ThemeProvider theme={{ ...MLL_THEME, ...theme }}>{children}</ThemeProvider>
+    <ThemeProvider theme={{ ...MLL_THEME, ...theme }}>
+      <AntdConfigProvider
+        prefixCls={PREFIX_CLS}
+      >
+        {children}
+      </AntdConfigProvider>
+    </ThemeProvider>
   );
 }
