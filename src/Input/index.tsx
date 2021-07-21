265f338e2909bf30a4276a661bb2d87a598ed1bc
Benedikt Franke <benedikt.franke@mll.com>
2021-07-21 17:43:08 +0200

    feat: link https://github.com/ant-design/ant-design/pull/31479

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -2,6 +2,7 @@ import { Input as AntdInput, InputNumber as AntdInputNumber } from 'antd';
 import { InputProps as AntdInputProps } from 'antd/lib/input';
 import { InputNumberProps as AntdInputNumberProps } from 'antd/lib/input-number';
 
+// Styles are broken due to https://github.com/ant-design/ant-design/pull/31479
 export const Input: typeof AntdInput = AntdInput;
 export type InputProps = AntdInputProps;
 
