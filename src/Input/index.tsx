d76df024f3dbaa3b2b83f22602e9df82805f5174
Benedikt Franke <benedikt.franke@mll.com>
2021-07-26 16:41:39 +0200

    feat: add NumericIDInput component

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -7,3 +7,5 @@ export type InputProps = AntdInputProps;
 
 export const InputNumber: typeof AntdInputNumber = AntdInputNumber;
 export type InputNumberProps = AntdInputNumberProps;
+
+export * from './NumericIDInput';
