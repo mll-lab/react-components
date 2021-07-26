b14a9aa5045a01a30c7bce389e944a355f1ee40b
Benedikt Franke <benedikt.franke@mll.com>
2021-07-26 16:41:39 +0200

    feat: add NumericIDInput component

---
+++ src/Input/NumericIDInput.tsx
@@ -0,0 +1,26 @@
+import { isOnlyDigits } from '@mll-lab/js-utils';
+import React, { useState } from 'react';
+
+import { Input, InputProps } from './index';
+
+export type NumericIDInputProps = InputProps;
+
+export function NumericIDInput(props: InputProps) {
+  const isControlled = 'value' in props;
+  const [valueState, setValueState] = useState('');
+
+  return (
+    <Input
+      onChange={(e) => {
+        const newValue = e.target.value;
+        if (isOnlyDigits(newValue)) {
+          props?.onChange?.(e);
+          if (!isControlled) {
+            setValueState(newValue);
+          }
+        }
+      }}
+      value={isControlled ? props.value : valueState}
+    />
+  );
+}
