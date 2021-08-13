2ec63d86105e3db553b9b949cbb38454bed4126c
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2021-08-13 13:42:54 +0200

    feat: add Checkbox props and Checkbox story

---
+++ src/Checkbox/index.stories.tsx
@@ -0,0 +1,20 @@
+import { Story } from '@storybook/react';
+import React from 'react';
+
+import { Checkbox, CheckboxProps } from './index';
+
+export default {
+  title: 'Checkbox',
+};
+
+export const Default: Story<CheckboxProps> = (args) => (
+  <Checkbox defaultChecked {...args}>
+    Checkbox
+  </Checkbox>
+);
+
+export const DisabledCheckbox: Story<CheckboxProps> = (args) => (
+  <Checkbox defaultChecked disabled {...args}>
+    Checkbox
+  </Checkbox>
+);
