1eb4edbc91ea1e543708b3ffee3b1f03a80fecd6
SimBig <SimBig@users.noreply.github.com>
2021-08-16 11:52:20 +0000

    refactor: Apply eslint fix

--- src/Collapse/index.tsx
+++ src/Collapse/index.tsx
@@ -1,4 +1,7 @@
-import { Collapse as AntdCollapse, CollapseProps as AntdCollapseProps } from 'antd';
+import {
+  Collapse as AntdCollapse,
+  CollapseProps as AntdCollapseProps,
+} from 'antd';
 
 export const Collapse: typeof AntdCollapse = AntdCollapse;
 export type CollapseProps = AntdCollapseProps;
