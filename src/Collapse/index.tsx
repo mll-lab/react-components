bc770ee7e1108d683da6a1bc21b53389f575f914
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-08-16 14:58:50 +0200

    feat: add CollapsibleTimelineItem component

--- src/Collapse/index.tsx
+++ src/Collapse/index.tsx
@@ -1,7 +1,9 @@
 import {
   Collapse as AntdCollapse,
   CollapseProps as AntdCollapseProps,
+  CollapsePanelProps as AntdCollapsePanelProps,
 } from 'antd';
 
 export const Collapse: typeof AntdCollapse = AntdCollapse;
 export type CollapseProps = AntdCollapseProps;
+export type CollapsePanelProps = AntdCollapsePanelProps;
