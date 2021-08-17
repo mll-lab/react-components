f18b8d762fb135341554d59374e595253419d033
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-08-17 10:34:57 +0200

    feat: add CollapsibleTimelineItem component

--- src/Timeline/index.stories.tsx
+++ src/Timeline/index.stories.tsx
@@ -1,7 +1,7 @@
 import { Story } from '@storybook/react';
 import React from 'react';
 
-import { Timeline, TimelineProps } from './index';
+import { CollapsibleTimelineItem, Timeline, TimelineProps } from './index';
 
 export default {
   title: 'Timeline',
@@ -9,6 +9,15 @@ export default {
 
 export const Default: Story<TimelineProps> = (args) => (
   <Timeline {...args}>
+    <CollapsibleTimelineItem collapsePanelProps={{ header: 'Header' }}>
+      Content
+    </CollapsibleTimelineItem>
+    <CollapsibleTimelineItem
+      collapseProps={{ defaultActive: true }}
+      collapsePanelProps={{ header: 'Header' }}
+    >
+      Content
+    </CollapsibleTimelineItem>
     <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
     <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
     <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
--- src/Timeline/index.tsx
+++ src/Timeline/index.tsx
@@ -1,7 +1,35 @@
 import {
   Timeline as AntdTimeline,
   TimelineProps as AntdTimelineProps,
+  TimelineItemProps as AntdTimelineItemProps,
 } from 'antd';
+import React, { ReactNode } from 'react';
+
+import { Collapse, CollapsePanelProps, CollapseProps } from '../Collapse';
 
 export const Timeline: typeof AntdTimeline = AntdTimeline;
 export type TimelineProps = AntdTimelineProps;
+export type TimelineItemProps = AntdTimelineItemProps;
+
+export function CollapsibleTimelineItem(props: {
+  timelineItemProps?: TimelineItemProps;
+  collapseProps?: Omit<CollapseProps, 'defaultActiveKey'> & {
+    defaultActive: boolean;
+  };
+  collapsePanelProps: Omit<CollapsePanelProps, 'key'>;
+  children: ReactNode;
+}) {
+  const key = 'default';
+  return (
+    <Timeline.Item {...props.timelineItemProps}>
+      <Collapse
+        defaultActiveKey={props.collapseProps?.defaultActive ? key : undefined}
+        {...props.collapseProps}
+      >
+        <Collapse.Panel key={key} {...props.collapsePanelProps}>
+          {props.children}
+        </Collapse.Panel>
+      </Collapse>
+    </Timeline.Item>
+  );
+}
