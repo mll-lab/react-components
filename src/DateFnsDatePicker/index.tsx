692bb22cc8d398756fa0fc3baccf086ba3f635ca
Benedikt Franke <benedikt.franke@mll.com>
2021-08-26 22:06:51 +0200

    clarify fix

--- src/DateFnsDatePicker/index.tsx
+++ src/DateFnsDatePicker/index.tsx
@@ -26,7 +26,7 @@ const localeParse = (format: string) =>
     .replace(/g/g, 'G')
     .replace(/([Ww])o/g, 'wo');
 
-// TODO fix https://github.com/react-component/picker/issues/147
+// TODO remove when https://github.com/react-component/picker/pull/289 fixes https://github.com/react-component/picker/issues/147
 export const BaseDatePicker = generatePicker<Date>({
   ...dateFnsGenerateConfig,
   locale: {
