58fc467424b200a4e22c5f38e89e97b3c82fa878
Benedikt Franke <benedikt.franke@mll.com>
2021-11-25 19:26:57 +0100

    feat: update dependencies

--- src/Skeleton/index.stories.tsx
+++ src/Skeleton/index.stories.tsx
@@ -9,3 +9,7 @@ export default {
 };
 
 export const Default: Story = (args) => <Skeleton {...args} />;
+export const Button: Story = (args) => <Skeleton.Button {...args} />;
+export const Input: Story = (args) => <Skeleton.Input {...args} />;
+export const Avatar: Story = (args) => <Skeleton.Avatar {...args} />;
+export const Image: Story = (args) => <Skeleton.Image {...args} />;
