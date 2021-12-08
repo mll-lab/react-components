54da09cfeaf2af4b46c9ed639d80715092d31528
Benedikt Franke <benedikt.franke@mll.com>
2021-12-08 12:48:45 +0100

    chore: remove dependency, define shades

--- src/theme.ts
+++ src/theme.ts
@@ -1,46 +1,58 @@
-import { gold, green, grey } from '@ant-design/colors';
 import { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 
 /* eslint-disable @mll-lab/no-color-literals */
+/**
+ * Inspired by https://ant.design/docs/spec/colors
+ */
 export const PALETTE = {
   red: '#ff0000', // matches the MLL corporate identity
-  gold: gold[5],
-  green: green[5],
-  lightBlue: '#d4edfc',
+  gold: '#ffc53d',
+  green: '#bae637',
   blue: '#50a0d0',
+  lightBlue: '#d4edfc',
 
+  // Shades
   white: '#ffffff', // brightest possible
+  lightGray: '#f0f0f0',
+  gray: '#8c8c8c',
+  darkGray: '#262626',
   black: '#000000', // darkest possible
 
-  grey: grey[5],
-  grey1: '#fafafa',
-  grey2: '#f0f2f5',
-  grey3: '#eaeaea',
-  grey4: '#dadada',
-  grey5: '#d3d3d3',
-  grey6: '#c0c0c0',
+  // TODO reduce to a minimal set of sufficiently different shades
+  /** @deprecated */
+  gray1: '#fafafa',
+  /** @deprecated */
+  gray2: '#f0f2f5',
+  /** @deprecated */
+  gray3: '#eaeaea',
+  /** @deprecated */
+  gray4: '#dadada',
+  /** @deprecated */
+  gray5: '#d3d3d3',
+  /** @deprecated */
+  gray6: '#c0c0c0',
 };
 /* eslint-enable @mll-lab/no-color-literals */
 
 export const THEME = {
   // Components
-  backgroundColor: PALETTE.grey2,
+  backgroundColor: PALETTE.gray2,
   borderColor: PALETTE.blue,
   collapseBackgroundColor: PALETTE.lightBlue,
-  containerBorderColor: PALETTE.grey5,
-  dividerColor: PALETTE.grey4,
+  containerBorderColor: PALETTE.gray5,
+  dividerColor: PALETTE.gray4,
   focusedRowColor: PALETTE.lightBlue,
-  menuGroupBackgroundColor: PALETTE.grey1,
-  panelBackgroundColor: PALETTE.grey3,
-  tableBorderColor: PALETTE.grey3,
-  titleColor: PALETTE.grey6,
+  menuGroupBackgroundColor: PALETTE.gray1,
+  panelBackgroundColor: PALETTE.gray3,
+  tableBorderColor: PALETTE.gray3,
+  titleColor: PALETTE.gray6,
 
   // UI states
   successColor: PALETTE.green,
   warningColor: PALETTE.gold,
   errorColor: PALETTE.red,
-  infoColor: PALETTE.grey4,
+  infoColor: PALETTE.gray4,
 };
 
 export type Theme = typeof THEME;
