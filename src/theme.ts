7999e81a3149cffb79a16434954b7a2be25a732d
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-12-08 15:55:19 +0100

    chore: reorder grey scale and map deprecated colors

--- src/theme.ts
+++ src/theme.ts
@@ -11,42 +11,41 @@ export const PALETTE = {
   green: '#bae637',
   blue: '#50a0d0',
   lightBlue: '#d4edfc',
+  blueTintedGray: '#f0f2f5',
 
   // Shades
   white: '#ffffff', // brightest possible
-  lightGray: '#f0f0f0',
-  gray: '#8c8c8c',
-  darkGray: '#262626',
-  black: '#000000', // darkest possible
-
-  // TODO reduce to a minimal set of sufficiently different shades
-  /** @deprecated */
   gray1: '#fafafa',
-  /** @deprecated */
-  gray2: '#f0f2f5',
-  /** @deprecated */
-  gray3: '#eaeaea',
-  /** @deprecated */
-  gray4: '#dadada',
-  /** @deprecated */
-  gray5: '#d3d3d3',
-  /** @deprecated */
-  gray6: '#c0c0c0',
+  gray2: '#f0f0f0',
+  gray3: '#e6e6e6',
+  gray4: '#d9d9d9',
+  gray5: '#c0c0c0',
+  gray6: '#8c8c8c',
+  gray7: '#595959',
+  gray8: '#434343',
+  gray9: '#262626',
+  black: '#000000', // darkest possible
+};
+
+/** @deprecated */
+const DEPRECATED_PALETTE = {
+  gray1: '#eaeaea',
+  gray2: '#d3d3d3',
 };
 /* eslint-enable @mll-lab/no-color-literals */
 
 export const THEME = {
   // Components
-  backgroundColor: PALETTE.gray2,
+  backgroundColor: PALETTE.blueTintedGray,
   borderColor: PALETTE.blue,
   collapseBackgroundColor: PALETTE.lightBlue,
-  containerBorderColor: PALETTE.gray5,
+  containerBorderColor: DEPRECATED_PALETTE.gray2,
   dividerColor: PALETTE.gray4,
   focusedRowColor: PALETTE.lightBlue,
   menuGroupBackgroundColor: PALETTE.gray1,
-  panelBackgroundColor: PALETTE.gray3,
+  panelBackgroundColor: DEPRECATED_PALETTE.gray1,
   tableBorderColor: PALETTE.gray3,
-  titleColor: PALETTE.gray6,
+  titleColor: PALETTE.gray5,
 
   // UI states
   successColor: PALETTE.green,
