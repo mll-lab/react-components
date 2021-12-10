e95b3b4496b5b1c785b579ffb058f2f4fde18038
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2021-12-10 17:06:11 +0100

    feat: support layout context in table

--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -2,28 +2,53 @@ import { Table as AntdTable, TableProps as AntdTableProps } from 'antd';
 import React, { ReactElement } from 'react';
 import styled from 'styled-components';
 
+import { useLayoutContext } from '../LayoutProvider';
+
 export { ColumnsType, ColumnProps } from 'antd/es/table';
 
 export type TableProps<RecordType> = AntdTableProps<RecordType>;
 
-export const StyledTable = styled(AntdTable)`
+export const StyledTable = styled(AntdTable)<{
+  // TODO: fix redundant prop type definitions
+  // Props starting with $ are transient props and will not be passed to component
+  $fontSize?: string;
+}>`
   ${(props) => {
     if (props.onRow?.({})?.onClick) {
       return `
         tbody tr:hover {
           cursor: pointer;
         }
+        .mll-ant-table {
+          font-size: ${(props.$fontSize || props.theme.fontSize) ?? undefined}
+        }
         `;
     }
 
-    return '';
+    return `
+      .mll-ant-table {
+        font-size: ${(props.$fontSize || props.theme.fontSize) ?? undefined}
+      }
+    `;
   }}
 ` as <RecordType extends Record<string, unknown> = Record<string, unknown>>(
-  props: TableProps<RecordType>,
+  props: TableProps<RecordType> & {
+    // Props starting with $ are transient props and will not be passed to component
+    $fontSize?: string;
+  },
 ) => ReactElement;
 
 export function Table<
   RecordType extends Record<string, unknown> = Record<string, unknown>,
 >(props: TableProps<RecordType>) {
-  return <StyledTable rowKey="id" {...props} />;
+  const { layout } = useLayoutContext();
+
+  return (
+    <StyledTable
+      size={layout.size ?? 'middle'}
+      $fontSize={layout.fontSize}
+      rowKey="id"
+      {...props}
+    />
+  );
 }
