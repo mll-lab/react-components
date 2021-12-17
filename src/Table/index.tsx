874b3b6d84999ab62c72d5777fa40f5e02920b1d
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2021-12-17 11:51:12 +0100

    feat: adapt table size and fontsize to theme

--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -1,12 +1,19 @@
 import { Table as AntdTable, TableProps as AntdTableProps } from 'antd';
+import { SizeType } from 'antd/es/config-provider/SizeContext';
 import React, { ReactElement } from 'react';
 import styled from 'styled-components';
+
+import { useTheme } from '../theme';
 
 export { ColumnsType, ColumnProps } from 'antd/es/table';
 
 export type TableProps<RecordType> = AntdTableProps<RecordType>;
 
 export const StyledTable = styled(AntdTable)`
+  .mll-ant-table {
+    font-size: ${(props) => props.theme.fontSize ?? undefined};
+  }
+
   ${(props) => {
     if (props.onRow?.({})?.onClick) {
       return `
@@ -25,5 +32,15 @@ export const StyledTable = styled(AntdTable)`
 export function Table<
   RecordType extends Record<string, unknown> = Record<string, unknown>,
 >(props: TableProps<RecordType>) {
-  return <StyledTable rowKey="id" {...props} />;
+  const { size } = useTheme();
+
+  return <StyledTable rowKey="id" size={mapSize(size)} {...props} />;
+}
+
+function mapSize(size: SizeType): SizeType {
+  // large is not designed by antd right now (though SizeType contains 'large')
+  if (size === 'large') {
+    return 'middle';
+  }
+  return size;
 }
