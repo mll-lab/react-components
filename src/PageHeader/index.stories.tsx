50558f53bbb38270a756eb4019d0963d69a94ba0
SimBig <SimBig@users.noreply.github.com>
2022-02-24 15:07:45 +0000

    refactor: Apply eslint fix

--- src/PageHeader/index.stories.tsx
+++ src/PageHeader/index.stories.tsx
@@ -2,16 +2,11 @@ import { Story } from '@storybook/react';
 import React from 'react';
 
 import { PageHeader, PageHeaderProps } from './index';
-import {Descriptions} from "../Descriptions";
 
 export default {
   title: 'PageHeader',
 };
 
 export const Default: Story<PageHeaderProps> = (args) => (
-  <PageHeader
-  title="Title"
-  subTitle="This is a subtitle"
-    {...args}
-  />
+  <PageHeader title="Title" subTitle="This is a subtitle" {...args} />
 );
