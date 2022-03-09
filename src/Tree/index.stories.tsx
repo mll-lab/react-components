4c5dd293b91bc621610df9c1e57766b1008f75de
Sebastian Gruenwald <sebastian.gruenwald@mll.com>
2022-03-09 09:38:31 +0100

    feat: Adding Tree component.

---
+++ src/Tree/index.stories.tsx
@@ -0,0 +1,23 @@
+import { Story } from '@storybook/react';
+import React from 'react';
+
+import { PALETTE } from '../theme';
+
+import { Tag, TagProps } from './index';
+
+export default {
+  title: 'Tag',
+  component: Tag,
+};
+
+export const HexColored: Story<TagProps> = (args) => (
+  <Tag color={PALETTE.gold} {...args}>
+    Test
+  </Tag>
+);
+
+export const PresetColored: Story<TagProps> = (args) => (
+  <Tag color="red" {...args}>
+    Test
+  </Tag>
+);
---
+++ src/Tree/index.tsx
@@ -0,0 +1,4 @@
+import { Tag as AntdTag, TagProps as AntdTagProps } from 'antd';
+
+export const Tag: typeof AntdTag = AntdTag;
+export type TagProps = AntdTagProps;
