84307b702f6ebdab2235e43a72d44c9f4af03e22
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2022-03-16 14:33:30 +0100

    refactor: use fontSize util

--- src/Tag/index.tsx
+++ src/Tag/index.tsx
@@ -6,16 +6,18 @@ import {
 import { CheckableTagProps as AntdCheckableTagProps } from 'antd/lib/tag/CheckableTag';
 import styled from 'styled-components';
 
+import { fontSizeFromTheme } from '../styled-utils';
+
 export type TagProps = AntdTagProps;
 export type TagType = AntdTagType;
 export type CheckableTagProps = AntdCheckableTagProps;
 
 const StyledTag: typeof AntdTag = styled(AntdTag)<TagProps>`
-  font-size: ${(props) => props.theme.fontSize};
+  font-size: ${fontSizeFromTheme};
 `;
 
 const StyledCheckableTag = styled(StyledTag.CheckableTag)<CheckableTagProps>`
-  font-size: ${(props) => props.theme.fontSize};
+  font-size: ${fontSizeFromTheme};
 `;
 
 export const Tag: typeof AntdTag = StyledTag;
