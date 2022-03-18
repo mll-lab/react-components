b639c6186bf297d4fe0c9b79a99b070cff8478a3
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2022-03-18 11:19:26 +0100

    fix: generic type definitions of forms and form items

--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -1,5 +1,6 @@
 import { Form as AntdForm, FormProps as AntdFormProps } from 'antd';
 import { FormItemProps as AntdFormItemProps } from 'antd/es/form/';
+import React from 'react';
 import styled from 'styled-components';
 
 import { fontSizeFromTheme } from '../styled-utils';
@@ -7,17 +8,23 @@ import { fontSizeFromTheme } from '../styled-utils';
 export type FormProps<T> = AntdFormProps<T>;
 export type FormItemProps<T> = AntdFormItemProps<T>;
 
-const StyledForm = styled(AntdForm)<FormProps<unknown>>`
+const StyledForm = styled(AntdForm)`
   font-size: ${fontSizeFromTheme};
-`;
+` as typeof AntdForm;
 
-const StyledFormItem = styled(StyledForm.Item)<FormItemProps<unknown>>`
+const StyledFormItem = styled(StyledForm.Item)`
   font-size: ${fontSizeFromTheme};
   .mll-ant-form-item-label > label {
     font-size: 1em;
   }
-`;
+` as typeof AntdForm.Item;
 
-export const Form: typeof AntdForm = StyledForm;
+export function Form<T = unknown>({ children, ...props }: FormProps<T>) {
+  return <StyledForm<T> {...props}>{children}</StyledForm>;
+}
 
-Form.Item = StyledFormItem;
+function FormItem<T = unknown>({ children, ...props }: FormItemProps<T>) {
+  return <StyledFormItem<T> {...props}>{children}</StyledFormItem>;
+}
+
+Form.Item = FormItem;
