de64a1b189d7ded028b074c29c9aa7a47cbce540
Simon Bigelmayr <simon.bigelmayr@mll.com>
2022-03-31 10:12:27 +0200

    refactor: move utils and types of Plate-component to new file

--- src/Plate/index.stories.tsx
+++ src/Plate/index.stories.tsx
@@ -3,13 +3,10 @@ import React from 'react';
 
 import { PALETTE } from '../theme';
 
-import {
-  COORDINATES_COLUMNS,
-  COORDINATES_ROWS,
-  Plate,
-  PlateProps,
-  PlateWell,
-} from './index';
+import { PlateProps, PlateWell } from './types';
+import { coordinatesForPosition } from './utils';
+
+import { COORDINATES_COLUMNS, COORDINATES_ROWS, Plate, WELLS } from './index';
 
 export default {
   title: 'Plate',
@@ -45,15 +42,51 @@ const data: Array<PlateWell> = [
   },
 ];
 
+const rowFlowData: Array<PlateWell> = WELLS.map((well) => ({
+  coordinates: coordinatesForPosition(well, 'row'),
+  content: well,
+}));
+
+const columnFlowData: Array<PlateWell> = WELLS.map((well) => ({
+  coordinates: coordinatesForPosition(well, 'column'),
+  content: well,
+}));
+
 const Template: Story<Partial<PlateProps>> = (args) => (
   <Plate data={null} {...args} />
 );
+
 export const Default = Template.bind({});
 Default.args = {
   data,
 };
+
+export const RowFlow = Template.bind({});
+RowFlow.args = {
+  data: rowFlowData,
+};
+
+export const ColumnFlow = Template.bind({});
+ColumnFlow.args = {
+  data: columnFlowData,
+};
+
 /* TODO: delete after https://github.com/storybookjs/storybook/issues/11554 is resolved */
+RowFlow.parameters = {
+  docs: {
+    source: {
+      code: 'Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554',
+    },
+  },
+};
 Default.parameters = {
+  docs: {
+    source: {
+      code: 'Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554',
+    },
+  },
+};
+ColumnFlow.parameters = {
   docs: {
     source: {
       code: 'Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554',
