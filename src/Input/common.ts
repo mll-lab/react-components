86c0e5845ca7c0dd541e698e4ba71f80bfdbee5a
kilya11 <kilya@ymail.com>
2022-04-06 13:49:15 +0200

    feat: export InputRef type (#159)

--- src/Input/common.ts
+++ src/Input/common.ts
@@ -3,12 +3,14 @@ import {
   InputNumber as AntdInputNumber,
   InputNumberProps as AntdInputNumberProps,
   InputProps as AntdInputProps,
+  InputRef as AntdInputRef,
 } from 'antd';
 import styled from 'styled-components';
 
 import { fontSizeFromTheme } from '../styled-utils';
 
 export type InputProps = AntdInputProps;
+export type InputRef = AntdInputRef;
 export type InputNumberProps = AntdInputNumberProps;
 
 export const Input: typeof AntdInput = styled(AntdInput)`
