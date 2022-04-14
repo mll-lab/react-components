708e876b178cb070ccd9c42b05dc3d1a9370daaf
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2022-04-14 16:17:48 +0200

    refactor: stricter typings

--- src/Space/index.tsx
+++ src/Space/index.tsx
@@ -1,9 +1,5 @@
 import { Space as AntdSpace, SpaceProps as AntdSpaceProps } from 'antd';
-import React from 'react';
-import styled from 'styled-components';
-
-import { PREFIX_CLS } from '../Provider';
-import { classNames } from '../classNames';
+import styled, { css } from 'styled-components';
 
 export { SpaceSize } from 'antd/lib/space';
 
@@ -12,22 +8,16 @@ export type SpaceProps = AntdSpaceProps & {
 };
 
 function shouldForwardProp(prop: keyof SpaceProps) {
-  return !['block'].includes(prop);
+  const transientProps: Array<keyof SpaceProps> = ['block'];
+  return !transientProps.includes(prop);
 }
 
-export const Space = styled(Space).withConfig<SpaceProps>({
+export const Space = styled(AntdSpace).withConfig<SpaceProps>({
   shouldForwardProp,
 })`
-  ${(props) => (props.block ? 'display: flex;' : '')}
+  ${(props) =>
+    props.block &&
+    css`
+      display: flex;
+    `};
 `;
-
-export function Space({ block, children, ...props }: SpaceProps) {
-  return (
-    <StyledSpace
-      {...props}
-      className={classNames([props.className, block && BLOCK_CLASS])}
-    >
-      {children}
-    </StyledSpace>
-  );
-}
