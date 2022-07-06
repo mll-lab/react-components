f05769ff815e20519a55ed43847fb9bb0a20d7fb
Benedikt Franke <benedikt.franke@mll.com>
2022-07-06 10:19:15 +0200

    fix: adapt Checkbox font-size from theme

--- src/Checkbox/index.tsx
+++ src/Checkbox/index.tsx
@@ -1,5 +1,9 @@
 import { Checkbox as AntdCheckbox } from 'antd';
+import styled from 'styled-components';
+import { fontSizeFromTheme } from '../styled-utils';
 
 export { CheckboxProps } from 'antd';
 
-export const Checkbox: typeof AntdCheckbox = AntdCheckbox;
+export const Checkbox: typeof AntdCheckbox = styled(AntdCheckbox)`
+  font-size: ${fontSizeFromTheme};
+`;
