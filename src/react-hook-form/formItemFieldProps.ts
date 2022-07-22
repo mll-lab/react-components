c4dc110905cb9bb8e35de3fc84f025e1a7357217
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2022-07-22 13:45:23 +0200

    feat: add formItemFieldProps util

---
+++ src/react-hook-form/formItemFieldProps.ts
@@ -0,0 +1,9 @@
+import { ControllerFieldState } from "react-hook-form";
+import { FormItemProps } from "../Form";
+
+export function formItemFieldProps(fieldState: ControllerFieldState): Partial<FormItemProps<unknown>> {
+  return {
+    hasFeedback: fieldState.invalid,
+    help: fieldState.error?.message,
+  };
+}
