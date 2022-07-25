c15d2025b9d267dd82907f5cc4d38990f5becccd
Ilja Kiel <ilja.kiel@mll.com>
2022-07-25 13:13:55 +0200

    feat: Add BackTop component

---
+++ src/BackTop/index.tsx
@@ -0,0 +1,4 @@
+import { BackTop as AntdBackTop, BackTopProps as AntdBackTopProps } from 'antd';
+
+export const BackTop: typeof AntdBackTop = AntdBackTop;
+export type BackTopProps = AntdBackTopProps;
