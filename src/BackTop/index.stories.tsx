6e21eb56bca87ad9ad1bbbd189aad6d0f7a07cb1
kilya11 <kilya@ymail.com>
2022-07-25 14:08:20 +0200

    feat: Add BackTop component (#177)
    Co-authored-by: Ilja Kiel <ilja.kiel@mll.com>

---
+++ src/BackTop/index.stories.tsx
@@ -0,0 +1,22 @@
+import { VerticalAlignTopOutlined } from '@ant-design/icons';
+import { Story } from '@storybook/react';
+import React from 'react';
+
+import { BackTop, BackTopProps } from './index';
+
+export default {
+  title: 'BackTop',
+};
+
+export const Default: Story<BackTopProps> = (args) => (
+  <div>
+    <div style={{ height: '1600px' }}>
+      Content height = 1600px
+      <br />
+      <br />
+      Scroll down to see{' '}
+      <VerticalAlignTopOutlined style={{ fontSize: '24px' }} /> bottom right.
+    </div>
+    <BackTop {...args} />
+  </div>
+);
