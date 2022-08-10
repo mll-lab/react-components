602b165bf1b11705827a2c54f1b594123cdd2733
mic-web <4804412+mic-web@users.noreply.github.com>
2022-08-10 09:59:20 +0200

    feat: align checkbox with form label (#179)
    * fix: align checkbox with form item label
    
    * refactor: Apply eslint fix
    
    Co-authored-by: mic-web <mic-web@users.noreply.github.com>

--- src/Checkbox/index.stories.tsx
+++ src/Checkbox/index.stories.tsx
@@ -1,6 +1,8 @@
 import { Story } from '@storybook/react';
 import React from 'react';
 
+import { Form } from '../Form';
+
 import { Checkbox, CheckboxProps } from './index';
 
 export default {
@@ -21,4 +23,12 @@ export const DisabledCheckbox: Story<CheckboxProps> = (args) => (
 
 export const WithoutLabel: Story<CheckboxProps> = (args) => (
   <Checkbox defaultChecked {...args} />
+);
+
+export const WithFormLabel: Story<CheckboxProps> = (args) => (
+  <Form>
+    <Form.Item label="Checkbox">
+      <Checkbox defaultChecked {...args} />
+    </Form.Item>
+  </Form>
 );
