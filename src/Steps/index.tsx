d1ce7d959b572008e3a0f22e270bb5baab339f2e
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2022-09-12 16:52:00 +0200

    fix: adapt steps to font size and center icon vertically

--- src/Steps/index.tsx
+++ src/Steps/index.tsx
@@ -3,7 +3,17 @@ import {
   StepsProps as AntdStepsProps,
   StepProps as AntdStepProps,
 } from 'antd';
+import styled from 'styled-components';
 
-export const Steps: typeof AntdSteps = AntdSteps;
+import { fontSizeFromTheme } from '../styled-utils';
+
 export type StepsProps = AntdStepsProps;
 export type StepProps = AntdStepProps;
+
+export const Steps: typeof AntdSteps = styled(AntdSteps)`
+  font-size: ${fontSizeFromTheme};
+
+  .mll-ant-steps-icon {
+    top: -5%;
+  }
+`;
