d51f4126ae323ab3ba16db74b294d49ca2ba9a32
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2022-09-26 14:12:24 +0200

    feat: export CheckboxChangeEvent

--- src/Checkbox/index.tsx
+++ src/Checkbox/index.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { fontSizeFromTheme } from '../styled-utils';
 
 export { CheckboxProps } from 'antd';
+export { CheckboxChangeEvent } from 'antd/lib/checkbox';
 
 export const Checkbox: typeof AntdCheckbox = styled(AntdCheckbox)`
   font-size: ${fontSizeFromTheme};
