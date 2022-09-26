bb4a96e765d1ca88fe3b06b4fbb62c5ac3f7c11e
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2022-09-26 15:01:25 +0200

    fix: format

--- src/Modal/index.tsx
+++ src/Modal/index.tsx
@@ -1,6 +1,6 @@
-import { ModalProps as AntdModalProps } from "antd/es/modal";
-import { Modal as AntdModal } from "antd";
-import styled from "styled-components";
+import { Modal as AntdModal } from 'antd';
+import { ModalProps as AntdModalProps } from 'antd/es/modal';
+import styled from 'styled-components';
 
 export type ModalProps = AntdModalProps;
 
@@ -11,8 +11,8 @@ export const Modal: typeof AntdModal = styled(AntdModal)`
   // Fixes antd bug where prefixes do not apply correctly on all classes.
   // Styles are copied from antd.
   .mll-ant-modal-footer
-  .mll-ant-btn
-  + .mll-ant-btn:not(.mll-ant-dropdown-trigger) {
+    .mll-ant-btn
+    + .mll-ant-btn:not(.mll-ant-dropdown-trigger) {
     margin-bottom: 0;
     margin-left: 8px;
   }
