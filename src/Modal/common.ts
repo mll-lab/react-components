6057366b9bc6f605e4910caa6eb76cbef65185e1
mic-web <mic-web@users.noreply.github.com>
2022-09-26 12:28:17 +0000

    refactor: Apply eslint fix

--- src/Modal/common.ts
+++ src/Modal/common.ts
@@ -1,9 +1,11 @@
-import { Modal as AntdModal } from "antd";
-import styled from "styled-components";
+import { Modal as AntdModal } from 'antd';
+import styled from 'styled-components';
 
-export const Modal: typeof AntdModal = styled(AntdModal)`  
+export const Modal: typeof AntdModal = styled(AntdModal)`
   // fixes antd bug where prefixes do not apply correctly on all classes
-  .mll-ant-modal-footer .mll-ant-btn + .mll-ant-btn:not(.mll-ant-dropdown-trigger) {
+  .mll-ant-modal-footer
+    .mll-ant-btn
+    + .mll-ant-btn:not(.mll-ant-dropdown-trigger) {
     margin-bottom: 0;
     margin-left: 8px;
   }
--- src/Modal/index.tsx
+++ src/Modal/index.tsx
@@ -2,6 +2,6 @@ import { ModalProps as AntdModalProps } from 'antd/es/modal';
 
 export type ModalProps = AntdModalProps;
 
-export * from './common'
+export * from './common';
 export * from './FullScreenModal';
 export * from './WithModal';
