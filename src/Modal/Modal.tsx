404f287f9b4dd05920deaed154f04fa423e8bd62
Benedikt Franke <benedikt.franke@mll.com>
2022-10-10 12:13:07 +0200

    refactor: avoid cyclic dependencies

---
+++ src/Modal/Modal.tsx
@@ -0,0 +1,4 @@
+import { Modal as AntdModal, ModalProps as AntdModalProps } from 'antd';
+
+export const Modal: typeof AntdModal = AntdModal;
+export type ModalProps = AntdModalProps;
--- src/Modal/index.tsx
+++ src/Modal/index.tsx
@@ -1,18 +1,3 @@
-import { Modal as AntdModal } from 'antd';
-import { ModalProps as AntdModalProps } from 'antd/es/modal';
-import styled from 'styled-components';
-
-export type ModalProps = AntdModalProps;
-export const Modal: typeof AntdModal = styled(AntdModal)`
-  // Fixes antd bug where prefixes do not apply correctly on all classes.
-  // Styles are copied from antd.
-  .mll-ant-modal-footer
-    .mll-ant-btn
-    + .mll-ant-btn:not(.mll-ant-dropdown-trigger) {
-    margin-bottom: 0;
-    margin-left: 8px;
-  }
-`;
-
+export * from './Modal';
 export * from './FullScreenModal';
 export * from './WithModal';
