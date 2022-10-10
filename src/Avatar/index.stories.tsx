acdbabd5b418222fdbe18f0d960a0c7f217fb1a1
Benedikt Franke <benedikt.franke@mll.com>
2022-10-10 12:12:21 +0200

    chore: fix top level arrow functions

--- src/Avatar/index.stories.tsx
+++ src/Avatar/index.stories.tsx
@@ -12,32 +12,36 @@ export default {
   title: 'Avatar',
 };
 
-export const Default: Story<AvatarProps> = (args) => (
-  <Avatar
-    style={{
-      backgroundColor: PALETTE.gold,
-      color: PALETTE.white,
-    }}
-    {...args}
-  >
-    Test
-  </Avatar>
-);
+export const Default: Story<AvatarProps> = function Default(args) {
+  return (
+    <Avatar
+      style={{
+        backgroundColor: PALETTE.gold,
+        color: PALETTE.white,
+      }}
+      {...args}
+    >
+      Test
+    </Avatar>
+  );
+};
 
-export const CustomizableUserAvatar: Story<UserAvatarProps> = (args) => (
-  <UserAvatar {...args} />
-);
+export const CustomizableUserAvatar: Story<UserAvatarProps> =
+  function CustomizableUserAvatar(args) {
+    return <UserAvatar {...args} />;
+  };
 
 CustomizableUserAvatar.argTypes = {
   username: { control: { type: 'text' }, defaultValue: 'ABC' },
 };
 
-export const VariableUserAvatars: Story<Omit<UserAvatarProps, 'username'>> = (
-  args,
-) => (
-  <>
-    {range(1, 50).map((i) => (
-      <UserAvatar key={i} username={randomString()} {...args} />
-    ))}
-  </>
-);
+export const VariableUserAvatars: Story<Omit<UserAvatarProps, 'username'>> =
+  function VariableUserAvatars(args) {
+    return (
+      <>
+        {range(1, 50).map((i) => (
+          <UserAvatar key={i} username={randomString()} {...args} />
+        ))}
+      </>
+    );
+  };
