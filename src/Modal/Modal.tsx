4f3e7e2ccdc0d8fa71c50dfd5bd30bede5e0256e
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2022-10-19 13:20:44 +0200

    fix: createGlobalStyle.withConfig is not a function

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import { Modal as AntdModal, ModalProps as AntdModalProps } from 'antd';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 
 export type ModalProps = AntdModalProps;
 export const Modal: typeof AntdModal = styled(AntdModal)`
@@ -13,7 +13,7 @@ export const Modal: typeof AntdModal = styled(AntdModal)`
   }
 `;
 
-export const ModalConfirmStyles = createGlobalStyle`
+export const ModalConfirmStyles = styled.prototype.createGlobalStyle`
   // Fixes antd bug where prefixes do not apply correctly on all classes.
   // Styles are copied from antd.
   .mll-ant-modal-confirm .mll-ant-modal-confirm-btns .mll-ant-btn + .mll-ant-btn {
