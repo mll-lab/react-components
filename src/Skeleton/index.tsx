39bde369f3e58e441fff29bb43e35c1315707937
Benedikt Franke <benedikt.franke@mll.com>
2023-01-20 12:56:46 +0100

    chore: fix Skeleton type

--- src/Skeleton/index.tsx
+++ src/Skeleton/index.tsx
@@ -2,6 +2,23 @@ import {
   Skeleton as AntdSkeleton,
   SkeletonProps as AntdSkeletonProps,
 } from 'antd';
+import SkeletonAvatar from 'antd/lib/skeleton/Avatar';
+import SkeletonButton from 'antd/lib/skeleton/Button';
+import SkeletonImage from 'antd/lib/skeleton/Image';
+import SkeletonInput from 'antd/lib/skeleton/Input';
+import SkeletonNode from 'antd/lib/skeleton/Node';
+import * as React from 'react';
 
-export const Skeleton: typeof AntdSkeleton = AntdSkeleton;
+// Copied from antd/lib/skeleton/Skeleton.d.ts to fix:
+// Default export of the module has or is using private name 'CompoundedComponent'.
+type CompoundedComponent = {
+  Button: typeof SkeletonButton;
+  Avatar: typeof SkeletonAvatar;
+  Input: typeof SkeletonInput;
+  Image: typeof SkeletonImage;
+  Node: typeof SkeletonNode;
+};
+
+export const Skeleton: React.FC<SkeletonProps> & CompoundedComponent =
+  AntdSkeleton;
 export type SkeletonProps = AntdSkeletonProps;
