491fddf76c26de56a5105e64347589423a08102a
Benedikt Franke <benedikt.franke@mll.com>
2023-01-20 12:31:10 +0100

    fix: actually expose Space.Compact

--- src/Space/index.tsx
+++ src/Space/index.tsx
@@ -1,4 +1,5 @@
 import { Space as AntdSpace, SpaceProps as AntdSpaceProps } from 'antd';
+import * as React from 'react';
 import styled, { css } from 'styled-components';
 
 export { SpaceSize } from 'antd/lib/space';
@@ -9,12 +10,16 @@ export type SpaceProps = AntdSpaceProps & {
 
 const TRANSIENT_PROPS: Array<keyof SpaceProps> = ['block'];
 
-function shouldForwardProp(prop: keyof SpaceProps) {
-  return !TRANSIENT_PROPS.includes(prop);
-}
+// Copied from antd/lib/space/index.d.ts to fix:
+// TS4023: Exported variable 'Space' has or is using name 'CompoundedComponent' from external module "/home/runner/work/react-components/react-components/node_modules/antd/lib/space/index" but cannot be named.
+type CompoundedComponent = {
+  Compact: typeof AntdSpace.Compact;
+} & React.FC<SpaceProps>;
 
-export const Space = styled(AntdSpace).withConfig<SpaceProps>({
-  shouldForwardProp,
+export const Space = styled<CompoundedComponent>(
+  AntdSpace,
+).withConfig<SpaceProps>({
+  shouldForwardProp: (prop) => !TRANSIENT_PROPS.includes(prop),
 })`
   ${(props) =>
     props.block &&
@@ -22,3 +27,4 @@ export const Space = styled(AntdSpace).withConfig<SpaceProps>({
       display: flex;
     `};
 `;
+Space.Compact = AntdSpace.Compact;
