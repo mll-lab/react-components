f4c9b598351caf60aa8591c0fddf086d2cd4c452
Benedikt Franke <benedikt.franke@mll.com>
2023-03-07 10:43:47 +0100

    feat: add function indeterminate

--- src/Checkbox/index.tsx
+++ src/Checkbox/index.tsx
@@ -6,6 +6,19 @@ import { fontSizeFromTheme } from '../styled-utils';
 export { CheckboxProps } from 'antd';
 export { CheckboxChangeEvent } from 'antd/lib/checkbox';
 
+/**
+ * Are there both truthy and falsy values in the given array of booleans?
+ */
+export function indeterminate(
+  booleans: Array<boolean | undefined | null> | undefined | null,
+): boolean {
+  if (!booleans) {
+    return false;
+  }
+
+  return booleans.some((value) => value) && booleans.some((value) => !value);
+}
+
 export const Checkbox: typeof AntdCheckbox = styled(AntdCheckbox)`
   font-size: ${fontSizeFromTheme};
   .mll-ant-checkbox {
