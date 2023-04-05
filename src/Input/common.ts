17c17712582b739640d1998db5ae509e1b83fde4
Benedikt Franke <benedikt.franke@mll.com>
2023-04-05 15:49:50 +0200

    feat: add SearchProps

--- src/Input/common.ts
+++ src/Input/common.ts
@@ -1,32 +1,33 @@
 import {
   Input as AntdInput,
-  InputNumber as AntdInputNumber,
-  InputNumberProps as AntdInputNumberProps,
   InputProps as AntdInputProps,
   InputRef as AntdInputRef,
+  InputNumber as AntdInputNumber,
+  InputNumberProps as AntdInputNumberProps,
 } from 'antd';
 import { TextAreaProps as AntdTextAreaProps } from 'antd/es/input';
+import { SearchProps as AntdSearchProps } from 'antd/es/input/Search';
 import { TextAreaRef as AntdTextAreaRef } from 'antd/es/input/TextArea';
 import styled from 'styled-components';
 
 import { fontSizeFromTheme } from '../styled-utils';
 
-export type InputProps = AntdInputProps;
-export type TextAreaProps = AntdTextAreaProps;
-export type InputRef = AntdInputRef;
-export type TextAreaRef = AntdTextAreaRef;
-export type InputNumberProps = AntdInputNumberProps;
-
 export const Input: typeof AntdInput = styled(AntdInput)`
   font-size: ${fontSizeFromTheme};
 `;
+export type InputProps = AntdInputProps;
+export type InputRef = AntdInputRef;
 
 export const InputNumber: typeof AntdInputNumber = styled(AntdInputNumber)`
   font-size: ${fontSizeFromTheme};
 `;
+export type InputNumberProps = AntdInputNumberProps;
 
 export const TextArea: typeof Input.TextArea = styled(Input.TextArea)`
   font-size: ${fontSizeFromTheme};
 `;
-
 Input.TextArea = TextArea;
+export type TextAreaProps = AntdTextAreaProps;
+export type TextAreaRef = AntdTextAreaRef;
+
+export type SearchProps = AntdSearchProps;
