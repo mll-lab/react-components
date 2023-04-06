33c7b55a3da37b838016b83ee15280bc88715bc4
Benedikt Franke <benedikt.franke@mll.com>
2023-04-06 14:08:31 +0200

    satisfies

--- src/Plate/constants.ts
+++ src/Plate/constants.ts
@@ -4,10 +4,10 @@ import { Coordinates, FlowDirection } from './types';
 
 const TUBE_COUNT = 96;
 export const WELLS = range(1, TUBE_COUNT + 1);
-export const COORDINATES_COLUMNS: ReadonlyArray<Coordinates['column']> = [
+export const COORDINATES_COLUMNS = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-] as const;
-export const COORDINATES_ROWS: ReadonlyArray<Coordinates['row']> = [
+] as const satisfies ReadonlyArray<Coordinates['column']>;
+export const COORDINATES_ROWS = [
   'A',
   'B',
   'C',
@@ -16,5 +16,5 @@ export const COORDINATES_ROWS: ReadonlyArray<Coordinates['row']> = [
   'F',
   'G',
   'H',
-] as const;
-export const PLATE_FLOW: FlowDirection = 'row' as const;
+] as const satisfies ReadonlyArray<Coordinates['row']>;
+export const PLATE_FLOW = 'row' as const satisfies FlowDirection;
