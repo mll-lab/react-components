460b0ea3160fa2c0e5cf1aa6022f7f81b79b47a0
Benedikt Franke <benedikt.franke@mll.com>
2023-04-14 14:08:28 +0200

    feat: introduce *ToColumnFilterItem utils
    BREAKING CHANGE: rename labeledEnumToColumnFilterItem to objectToColumnFilterItem

--- src/Table/utils.test.ts
+++ src/Table/utils.test.ts
@@ -1,19 +1,41 @@
-import { labeledEnumToColumnFilterItem } from './utils';
+import { mapToColumnFilterItem, objectToColumnFilterItem } from './utils';
 
 enum Foo {
   BAR = 'baz',
 }
 
-const LabeledFoo = {
+const FOO_LABELS = {
   [Foo.BAR]: 'foobar',
 };
 
-describe('labeledEnumToColumnFilterItem', () => {
-  it('converts an enum', () => {
-    expect(labeledEnumToColumnFilterItem(LabeledFoo)).toEqual([
+describe('objectToColumnFilterItem', () => {
+  it('converts a labeled enum', () => {
+    expect(objectToColumnFilterItem(FOO_LABELS)).toEqual([
       {
         text: 'foobar',
         value: 'baz',
+      },
+    ]);
+  });
+});
+
+describe('mapToColumnFilterItem', () => {
+  it('converts a map with boolean keys', () => {
+    expect(
+      mapToColumnFilterItem(
+        new Map([
+          [true, 'Yes'],
+          [false, 'No'],
+        ]),
+      ),
+    ).toEqual([
+      {
+        value: true,
+        text: 'Yes',
+      },
+      {
+        value: false,
+        text: 'No',
       },
     ]);
   });
