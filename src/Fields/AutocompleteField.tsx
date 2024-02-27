b6815314ad031b794f7ffa3f3e66cb19f5520a13
Benedikt Franke <benedikt.franke@mll.com>
2024-02-27 17:09:00 +0100

    feat: add AutocompleteField

---
+++ src/Fields/AutocompleteField.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  useController,
+  FieldValues,
+  UseControllerProps,
+  FieldPath,
+} from 'react-hook-form';
+
+import { Autocomplete, AutocompleteProps } from '../Autocomplete';
+
+import { useFieldContext } from './FieldProvider';
+import { FieldWrapper, FieldWrapperProps } from './FieldWrapper';
+
+type AutocompleteFieldProps<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>,
+> = UseControllerProps<TFieldValues, TName> &
+  Pick<FieldWrapperProps<TFieldValues, TName>, 'formItem'> & {
+    component?: AutocompleteProps;
+  };
+
+/**
+ * This component appears unstyled in Storybook because it is not properly prefixing its class names.
+ * However, if this library is used alongside globally included antd styles, it works somewhat fine.
+ * When we rip out antd 3 from our projects, we may want to remove the class prefix in this library to fix this.
+ */
+export function AutocompleteField<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  formItem,
+  component,
+  ...controller
+}: AutocompleteFieldProps<TFieldValues, TName>) {
+  const { field } = useController<TFieldValues, TName>(controller);
+
+  const { disabled } = useFieldContext();
+
+  return (
+    <FieldWrapper controller={controller} formItem={formItem}>
+      <Autocomplete
+        {...field}
+        value={field.value ?? undefined}
+        disabled={disabled}
+        {...component}
+      />
+    </FieldWrapper>
+  );
+}
