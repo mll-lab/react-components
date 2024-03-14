d8b961fb6aabdac5b23029abf6eb1f6937b908ef
Christian Seidel <christian.seidel@mll.com>
2024-03-14 15:23:38 +0100

    feat: add DatePickerField.tsx

---
+++ src/Fields/DatePickerField.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  useController,
+  FieldPath,
+  FieldValues,
+  UseControllerProps,
+} from 'react-hook-form';
+
+import { DatePicker, DatePickerProps } from '../DatePicker';
+
+import { useFieldContext } from './FieldProvider';
+import { FieldWrapper, FieldWrapperProps } from './FieldWrapper';
+
+type DatePickerFieldProps<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>,
+> = UseControllerProps<TFieldValues, TName> &
+  Pick<FieldWrapperProps<TFieldValues, TName>, 'formItem'> & {
+    component?: DatePickerProps;
+  };
+
+export function DatePickerField<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  formItem,
+  component,
+  ...controller
+}: DatePickerFieldProps<TFieldValues, TName>) {
+  const { field } = useController<TFieldValues, TName>(controller);
+
+  const { disabled } = useFieldContext();
+
+  return (
+    <FieldWrapper controller={controller} formItem={formItem}>
+      <DatePicker disabled={disabled} {...field} {...component} />
+    </FieldWrapper>
+  );
+}
