2b27c2516f61ef4d4e69ef44ff188f51f98a3333
kilya11 <kilya@ymail.com>
2024-05-22 13:38:23 +0200

    feat: add callto link to user details

--- src/UserDetailsPopover/UserDetails.tsx
+++ src/UserDetailsPopover/UserDetails.tsx
@@ -1,62 +1,67 @@
-import { joinNonEmpty } from '@mll-lab/js-utils';
+import {joinNonEmpty} from '@mll-lab/js-utils';
 import React from 'react';
 
-import { UserAvatar } from '../Avatar';
-import { Divider } from '../Divider';
-import { Col, Row } from '../Grid';
-import { Space } from '../Space';
-import { Typography } from '../Typography';
+import {UserAvatar} from '../Avatar';
+import {Divider} from '../Divider';
+import {Col, Row} from '../Grid';
+import {Space} from '../Space';
+import {Typography} from '../Typography';
 
-import { EmailRow } from './EmailRow';
-import { GUTTER, SPACE } from './constants';
+import {EmailRow} from './EmailRow';
+import {GUTTER, SPACE} from './constants';
+import {PhoneRow} from "./PhoneRow";
 
 type User = {
-  acronym?: string | null;
-  email?: string | null;
-  firstname?: string | null;
-  inactive?: boolean;
-  lastname?: string | null;
-  username: string;
+    acronym?: string | null;
+    email?: string | null;
+    firstname?: string | null;
+    inactive?: boolean;
+    lastname?: string | null;
+    phone?: string | null;
+    username: string;
 };
 
 export type UserDetailsProps = {
-  user: User;
+    user: User;
 };
 
-export function UserDetails({ user }: UserDetailsProps) {
-  const { acronym, email, firstname, lastname, inactive, username } = user;
-  const fullName = joinNonEmpty([firstname, lastname], ' ');
-  const showFullName = !inactive && fullName;
-  return (
-    <Space vertical size={SPACE}>
-      <Row gutter={GUTTER} align="middle">
-        <Col flex="0 1 min-content">
-          <UserAvatar username={acronym} />
-        </Col>
-        <Col
-          flex="auto"
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-          }}
-        >
-          {showFullName ? (
-            <>
-              <Typography.Text strong>{fullName}</Typography.Text>
-              <Typography.Text>{username}</Typography.Text>
-            </>
-          ) : (
-            <Typography.Text strong>{username}</Typography.Text>
-          )}
-        </Col>
-      </Row>
+export function UserDetails({user}: UserDetailsProps) {
+    const {acronym, email, firstname, lastname, inactive, username, phone} = user;
+    const fullName = joinNonEmpty([firstname, lastname], ' ');
+    const showFullName = !inactive && fullName;
+    return (
+        <Space vertical size={SPACE}>
+            <Row gutter={GUTTER} align="middle">
+                <Col flex="0 1 min-content">
+                    <UserAvatar username={acronym}/>
+                </Col>
+                <Col
+                    flex="auto"
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'flex-start',
+                    }}
+                >
+                    {showFullName ? (
+                        <>
+                            <Typography.Text strong>{fullName}</Typography.Text>
+                            <Typography.Text>{username}</Typography.Text>
+                        </>
+                    ) : (
+                        <Typography.Text strong>{username}</Typography.Text>
+                    )}
+                </Col>
+            </Row>
 
-      <Divider style={{ marginTop: SPACE, marginBottom: SPACE }} />
+            <Divider style={{marginTop: SPACE, marginBottom: SPACE}}/>
 
-      {inactive
-        ? 'The user was deactivated'
-        : email && <EmailRow email={email} />}
-    </Space>
-  );
+            {inactive
+                ? 'The user was deactivated'
+                : <>
+                    {email && <EmailRow email={email}/>}
+                    {phone && <PhoneRow phone={phone}/>}
+                </>}
+        </Space>
+    );
 }
