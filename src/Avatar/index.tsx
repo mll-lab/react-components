7b2c8e4e463534dde742364a3bca75c6890cb149
Simon Bigelmayr <simon.bigelmayr@mll.com>
2024-08-20 11:22:11 +0200

    refactor: use `stringToHslaColor` from '@mll-lab/js-utils'

--- src/Avatar/index.tsx
+++ src/Avatar/index.tsx
@@ -1,8 +1,7 @@
 import { UserOutlined } from '@ant-design/icons';
+import { stringToHslaColor } from '@mll-lab/js-utils';
 import { Avatar as AntdAvatar, AvatarProps as AntdAvatarProps } from 'antd';
 import React from 'react';
-
-import { stringToHslaColor } from './utils';
 
 export const Avatar: typeof AntdAvatar = AntdAvatar;
 export type AvatarProps = AntdAvatarProps;
