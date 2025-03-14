24a8140c2d2f8ca0f6df2ef023405e8896af74b6
Michael Adlfinger <{ID}+{username}@users.noreply.github.com>
2025-03-14 10:20:34 +0100

    feat: let descriptions use theme font size

--- src/Descriptions/index.tsx
+++ src/Descriptions/index.tsx
@@ -2,6 +2,23 @@ import {
   Descriptions as AntdDescriptions,
   DescriptionsProps as AntdDescriptionsProps,
 } from 'antd';
+import styled from 'styled-components';
 
-export const Descriptions = AntdDescriptions;
+import { fontSizeFromTheme } from '../styled-utils';
+
+const StyledDescriptions = styled(AntdDescriptions)`
+  .mll-ant-descriptions-header {
+    margin-bottom: 4px;
+  }
+  .mll-ant-descriptions-title {
+    font-size: calc(${fontSizeFromTheme} + 1px);
+    font-weight: 700;
+  }
+  .mll-ant-descriptions-item-label,
+  .mll-ant-descriptions-item-content {
+    font-size: ${fontSizeFromTheme};
+  }
+`;
+
+export const Descriptions: typeof AntdDescriptions = StyledDescriptions;
 export type DescriptionsProps = AntdDescriptionsProps;
