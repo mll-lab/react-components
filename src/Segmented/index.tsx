d23cfe2a87429d9f15518472e0b75a6318e109a2
Quis90 <45073834+Quis90@users.noreply.github.com>
2025-11-13 13:57:51 +0000

    refactor: Apply eslint fix

--- src/Segmented/index.tsx
+++ src/Segmented/index.tsx
@@ -1,7 +1,10 @@
-import {Segmented as AntdSegmented, SegmentedProps as AntdSegmentedProps} from 'antd';
 import {
-    SegmentedValue as AntdSegmentedValue,
-    SegmentedLabeledOption as AntdSegmentedLabeledOption
+  Segmented as AntdSegmented,
+  SegmentedProps as AntdSegmentedProps,
+} from 'antd';
+import {
+  SegmentedValue as AntdSegmentedValue,
+  SegmentedLabeledOption as AntdSegmentedLabeledOption,
 } from 'antd/es/segmented';
 
 export type SegmentedValue = AntdSegmentedValue;
@@ -9,4 +12,3 @@ export type SegmentedLabeledOption = AntdSegmentedLabeledOption;
 
 export type SegmentedProps = AntdSegmentedProps;
 export const Segmented = AntdSegmented;
-
