75abd258d3b504a189481cfa64302f80a8a0ea01
Chris <45073834+Quis90@users.noreply.github.com>
2025-11-13 16:20:27 +0100

    feat: add component `Segmented`

---
+++ src/Segmented/index.tsx
@@ -0,0 +1,14 @@
+import {
+  Segmented as AntdSegmented,
+  SegmentedProps as AntdSegmentedProps,
+} from 'antd';
+import {
+  SegmentedValue as AntdSegmentedValue,
+  SegmentedLabeledOption as AntdSegmentedLabeledOption,
+} from 'antd/es/segmented';
+
+export type SegmentedValue = AntdSegmentedValue;
+export type SegmentedLabeledOption = AntdSegmentedLabeledOption;
+
+export type SegmentedProps = AntdSegmentedProps;
+export const Segmented = AntdSegmented;
