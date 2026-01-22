498539960748d2f382ee1b4a796f197c36dcdbe5
Ralf77 <35593596+Ralf77@users.noreply.github.com>
2026-01-22 13:30:10 +0100

    Refactor Upload component import paths

--- src/Upload/index.tsx
+++ src/Upload/index.tsx
@@ -1,8 +1,10 @@
 import {
   Upload as AntdUpload,
   UploadProps as AntdUploadProps,
-  RcFile as AntdRcFile,
 } from 'antd';
+import {
+ RcFile as AntdRcFile,
+} from 'antd/es/upload';
 import { UploadFile as AntdUploadFile } from 'antd/lib/upload/interface';
 
 export const Upload: typeof AntdUpload = AntdUpload;
