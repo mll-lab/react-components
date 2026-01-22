4481cd27af621e2cffb50ad084f496a01e6800d4
Ralf77 <35593596+Ralf77@users.noreply.github.com>
2026-01-22 13:23:43 +0100

    feat: Add RcFile type from Ant Design Upload

--- src/Upload/index.tsx
+++ src/Upload/index.tsx
@@ -1,6 +1,7 @@
-import { Upload as AntdUpload, UploadProps as AntdUploadProps } from 'antd';
+import { Upload as AntdUpload, UploadProps as AntdUploadProps, RcFile as AntdRcFile } from 'antd';
 import { UploadFile as AntdUploadFile } from 'antd/lib/upload/interface';
 
 export const Upload: typeof AntdUpload = AntdUpload;
 export type UploadProps = AntdUploadProps;
 export type UploadFile = AntdUploadFile;
+export type RcFile = AntdRcFile;
