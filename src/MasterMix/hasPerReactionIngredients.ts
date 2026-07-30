fc34e9f2c277e23fc4badbb5104e6f9346cdbdc2
Simon Bigelmayr <simon.bigelmayr@mll.com>
2026-07-30 21:05:16 +0200

    docs(MasterMix): let the predicate name speak for itself

--- src/MasterMix/hasPerReactionIngredients.ts
+++ src/MasterMix/hasPerReactionIngredients.ts
@@ -1,6 +1,5 @@
 import { MasterMixIngredient, ReactionMix } from './types';
 
-/** Without them, the reaction mix consists of nothing but the master mix. */
 export function hasPerReactionIngredients(
   perReactionIngredients: ReactionMix['perReactionIngredients'],
 ): perReactionIngredients is Array<MasterMixIngredient> {
